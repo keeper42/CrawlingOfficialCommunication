f2b6f0384e839f34ac66a4e9b5c6773960a937d0
keepfriend <szuljf@163.com>
2017-03-26 15:18:39 +0800

    search.html

--- remind.php
+++ remind.php
@@ -3,9 +3,9 @@
  * User: LJF
  * Date: 2017/3/25
  */
-    header("Content-type: text/html; charset=gb2312");
     // Refresh webpage.
     header("refresh:600; url=remind.php");
+    header("Content-type: text/html; charset=gb2312");
 
     // Local database.
     $hostname = 'localhost';
@@ -57,11 +57,11 @@
 
   			// If the query is successful then insert the information into the form.
   			if($flag) {
-  			    echo 'search success </br>';
+  			    echo ' search success </br>';
   			}else{ 	
   				$query  = "INSERT IGNORE INTO `remind` (`category`,`department`,`title`,`date`,`url`) VALUES ('".$category[1][0]."','".$department[1][0]."','".$titletxt."','{$date[1][2]}','".$vurl."')";
   			    if(mysqli_query($link, $query)) {
-  			        echo 'insert success';
+  			        echo ' insert success';
   			    }else{
   			        echo mysqli_error($link);
   			    }
@@ -69,12 +69,26 @@
   			}
   			echo '</br>'; 	
 
-  			// Set a reminder
-  		
+  			// Set a reminder...
+  			
   		}	
 
     }
+    // Search the keyword;
+    $key = urldecode($_POST['key']); 
+    $keyword = iconv("utf-8", "gb2312", $key);
+    if($key) {
+        $search_key = "SELECT * FROM `remind` WHERE `category` LIKE '%$keyword%' OR `department` LIKE '%$keyword%' OR `title` LIKE '%$keyword%' ";
+        $query_key = mysqli_query($link, $search_key);  
+        while($array_key = mysqli_fetch_array($query_key)) {  
+        	for($i = 0; $i < 5; $i++){
+        		echo $array_key[$i];
+        		echo '&nbsp&nbsp';
+        	}
+            echo '</br>';
+        }
+
+    }  
 
     mysqli_close($link);
-    
 ?>
---
+++ search.php
@@ -0,0 +1,33 @@
+<!-- Created by LJF on 2017/03/26 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>荔知公文通提醒</title>
+    <style>
+        body{
+            margin: auto 0;
+            text-align: center;
+            padding: 10% 0;
+        }
+    </style>
+</head>
+<body>
+
+<div class="s_form_wrapper">
+	<h1>荔知公文通提醒</h1>
+    <form id="form" action="remind.php" method="post">
+        <span>
+            <input id="id" name="key" value="" maxlength="255" autocomplete="off"
+                   style="width:40%; height:30px">
+        </span>
+        <span>
+            <input type="submit" id="submit" value="搜索一下" style="height:35px">
+        </span>
+    </form>
+
+</div>
+
+</body>
+</html>
